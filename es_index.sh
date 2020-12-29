a204cc0301946fba8ab10229c31416170dc42a71
Taeheon Kwon <kwontaeheon@gmail.com>
2020-12-29 16:42:43 +0900

    es index changed

--- es_index.sh
+++ es_index.sh
@@ -4,7 +4,6 @@ PUT - 데이터 수정
 DELETE - 데이터 제거
 
 
-
 DELETE maumilgi-diary-contents
 
 
@@ -13,15 +12,23 @@ PUT maumilgi-diary-contents
   "settings": {
     "analysis": {
       "analyzer": {
-        "nori_mixed": {
-          "tokenizer": "nori_t_mixed",
-          "filter": "my_shingle"
+        "nori_shingle": {
+          "tokenizer": "my_nori_tokenizer",
+          "filter": ["my_shingle"]
+        },
+        "nori_keywords": {
+          "tokenizer": "my_nori_discard",
+          "filter": ["my_shingle", "my_posfilter"]
         }
       },
       "tokenizer": {
-        "nori_t_mixed": {
+        "my_nori_tokenizer": {
           "type": "nori_tokenizer",
           "decompound_mode": "mixed"
+        },
+        "my_nori_discard": {
+          "type": "nori_tokenizer",
+          "decompound_mode": "discard"
         }
       },
       "filter": {
@@ -29,6 +36,11 @@ PUT maumilgi-diary-contents
           "type": "shingle",
           "token_separator": "",
           "max_shingle_size": 4
+        },
+        "my_posfilter": {
+          "type": "nori_part_of_speech", 
+          "stoptags": ["E", "IC","J","MAG", "MAJ", "MM", "SP", "SSC", "SSO", "SC", "SE", "XPN", "XSA", "XSN", "XSV", "UNA", "NA", "VSV",
+          "VCP", "VA", "VV", "NNB", "XSN", "MM", "VX","VA", "VCN"]
         }
       },
       "number_of_shards": 5,
@@ -42,30 +54,57 @@ PUT maumilgi-diary-contents
           "store": "true"
           
         },
-            "email": {
-		 "type": "text"
-	  },
+     "email": {
+		    "type": "text"
+	     },
       "title": {
         "type": "text",
-        "analyzer": "nori_mixed",
-        "search_analyzer": "standard"
+        "analyzer": "nori_shingle",
+        "search_analyzer": "standard",
+        "fields": {
+          "keywords": {
+            "type": "text",
+            "analyzer": "nori_keywords",
+            "fielddata": "true"    
+          }
+        }
+        
       },
       "contents": {
         "type": "text",
-        "analyzer": "nori_mixed",
-        "search_analyzer": "standard"
+        "analyzer": "nori_shingle",
+        "search_analyzer": "standard",
+        "fields": {
+          "keywords": {
+            "type": "text",
+            "analyzer": "nori_keywords",
+            "fielddata": "true"    
+          }
+        }
       }
     }
-          }
+  }
 }
         
 
 
 POST _bulk
 {"index":{"_index":"maumilgi-diary-contents"}}
-{"@timestamp": "2020-12-28T13:26:26.625Z","email": "kwontaeheon@gmail.com","name": "Taeheon Kwon", "title": "나의 첫번째 이야기", "contents": "나의 첫번째 글쓰기 입니다."}
+{"@timestamp": "2020-12-29T03:43:26.625Z","email": "kwontaeheon@gmail.com","name": "Taeheon Kwon", "title": "나의 세번째 이야기", "contents": "나의 세번째 글쓰기 입니다."}
 {"index":{"_index":"maumilgi-diary-contents"}}
-{"@timestamp": "2020-12-28T13:27:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 두번째 이야기",  "contents": "나의 두번째 글쓰기 입니다." }
+{"@timestamp": "2020-12-29T03:44:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 네번째 이야기",  "contents": "나의 네번째 글쓰기 입니다." }
+{"index":{"_index":"maumilgi-diary-contents"}}
+{"@timestamp": "2020-12-29T03:44:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 다섯번째 이야기",  "contents": "나의 다섯번째 글쓰기 입니다." }
+{"index":{"_index":"maumilgi-diary-contents"}}
+{"@timestamp": "2020-12-29T03:44:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 여섯번째 이야기",  "contents": "나의 여섯번째 글쓰기 입니다." }
+{"index":{"_index":"maumilgi-diary-contents"}}
+{"@timestamp": "2020-12-29T03:44:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 일곱번째 이야기",  "contents": "나의 일곱번째 글쓰기 입니다." }
+{"index":{"_index":"maumilgi-diary-contents"}}
+{"@timestamp": "2020-12-29T03:44:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 여덟번째 이야기",  "contents": "나의 여덟번째 글쓰기 입니다." }
+{"index":{"_index":"maumilgi-diary-contents"}}
+{"@timestamp": "2020-12-29T03:44:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 아홉번째 이야기",  "contents": "나의 아홉번째 글쓰기 입니다." }
+{"index":{"_index":"maumilgi-diary-contents"}}
+{"@timestamp": "2020-12-29T03:44:26.625Z","email": "kwontaeheon@gmail.com", "name": "Taeheon Kwon", "title": "나의 열번째 이야기",  "contents": "나의 열번째 글쓰기 입니다." }
 
 
 GET maumilgi-diary-contents/_search 
@@ -102,7 +141,6 @@ GET maumilgi-diary-contents/_search
   }
 }
 
-GET maumilgi-diary-contents/_termvectors/Vg1LqXYBa0Zs_6PFEORo?fields=contents
 
 
-
+GET maumilgi-diary-contents/_termvectors/pQ5xrXYBa0Zs_6PFPrnd?fields=contents
